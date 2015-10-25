32d8527b6bce0aa394445bacb0e976238b2c9d76
Albert Itaev <ijohnnyzzz@gmail.com>
2015-10-25 11:02:42 +0000

    Create basic Product model, controller and views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,33 @@
+class ProductsController < ApplicationController
+  before_action :find_product, only: [:show]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+end
